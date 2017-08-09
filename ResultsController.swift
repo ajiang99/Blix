5e15740659334841dbfa352b0a37004e512f4ebc
ajiang99 <andrewjiang99@gmail.com>
2017-08-08 21:28:45 -0400

    Cells Set

--- ResultsController.swift
+++ ResultsController.swift
@@ -37,20 +37,24 @@ class ResultsController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     var partyDrinkArr = [Drink]()
     
-    var softDrink = [Drink]()
+    var softDrinkArr = [Drink]()
     
+   var sections = [Section]()
+/*
     var sections = [
         Section(type: "Shot",
                 drinks: ["Fireball"],
                 expanded: false
             )
     ]
+ */
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for drink in arrDrinks{
-            print (drink.name)
-        }
+        
+        self.organizeDrinks()
+        self.createSections()
 
         // Do any additional setup after loading the view.
     }
@@ -61,38 +65,141 @@ class ResultsController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func organizeDrinks(){
-        func organizeDrinks(){
-            for drink in arrDrinks{
-                switch drink.type{
-                    
-                case "Shot":
-                    shotArr.append(drink)
-                case "Cocktail":
-                    cocktailArr.append(drink)
-                case "Beer":
-                    beerArr.append(drink)
-                case "Cocoa":
-                    cocoaArr.append(drink)
-                case "Coffee / Tea":
-                    coffeeTeaArr.append(drink)
-                case "Homemade Liqueur":
-                    homemadeLiqeuerArr.append(drink)
-                case "Ordinary Drink":
-                    ordinaryDrinkArr.append(drink)
-                case "Milk / Float / Shake":
-                    shakeArr.append(drink)
-                case "Other/Unknown":
-                    otherArr.append(drink)
-                case "Punch / Party Drink":
-                    partyDrinkArr.append(drink)
-                case "Soft Drinks and Soda":
-                    softDrink.append(drink)
-                default:
-                    break
-                }
+        for drink in arrDrinks{
+            switch drink.type{
+            case "Shot":
+                shotArr.append(drink)
+            case "Cocktail":
+                cocktailArr.append(drink)
+            case "Beer":
+                beerArr.append(drink)
+            case "Cocoa":
+                cocoaArr.append(drink)
+            case "Coffee / Tea":
+                coffeeTeaArr.append(drink)
+            case "Homemade Liqueur":
+                homemadeLiqeuerArr.append(drink)
+            case "Ordinary Drink":
+                ordinaryDrinkArr.append(drink)
+            case "Milk / Float / Shake":
+                shakeArr.append(drink)
+            case "Other/Unknown":
+                otherArr.append(drink)
+            case "Punch / Party Drink":
+                partyDrinkArr.append(drink)
+            case "Soft Drinks and Soda":
+                softDrinkArr.append(drink)
+            default:
+                break
             }
         }
     }
+    
+    func createSections(){
+        
+        if !shotArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in shotArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Shot (\(shotArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+        
+        if !cocktailArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in cocktailArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Cocktail (\(cocktailArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        
+        if !coffeeTeaArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in coffeeTeaArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Coffee or Tea (\(coffeeTeaArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        
+        if !cocoaArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in cocoaArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Cocoa (\(cocoaArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        if !homemadeLiqeuerArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in homemadeLiqeuerArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Homemade Liqeuer (\(homemadeLiqeuerArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        if !ordinaryDrinkArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in ordinaryDrinkArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Ordinary Drink (\(ordinaryDrinkArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        if !shakeArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in shakeArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Milk, Float, or Shake (\(shakeArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        if !partyDrinkArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in partyDrinkArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Party Drink (\(partyDrinkArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+
+        if !softDrinkArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in softDrinkArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Soft Drink (\(softDrinkArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+        
+        if !otherArr.isEmpty{
+            var drinkNameArr = [String]()
+            for drink in otherArr{
+                drinkNameArr.append(drink.name)
+            }
+            let drinkSection = Section(type: "Other (\(otherArr.count))", drinks: drinkNameArr, expanded: false)
+            
+            sections.append(drinkSection)
+        }
+    }
+ 
     /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
